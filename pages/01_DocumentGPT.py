6e38a82d580a283533f7c1e8f7f29b08c532add3
paqj <changraven@gmail.com>
2024-04-02 09:15:01 +0900

    modify errors

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -57,7 +57,7 @@ def embed_file(file):
         chunk_size=600,
         chunk_overlap=100,
     )
-    loader = UnstructuredFileLoader("./files/chapter_one.txt")
+    loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
 
     # embedding
