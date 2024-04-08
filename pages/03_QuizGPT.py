0194b31d367ba225b644ec4479af2c68e0a31e9e
paqj <changraven@gmail.com>
2024-04-08 21:42:52 +0900

    # 9.3 Question Propmpt

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -3,6 +3,8 @@ from langchain.retrievers import WikipediaRetriever
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
 
 st.set_page_config(
     page_title="QuizGPT",
@@ -16,6 +18,9 @@ st.title("QuizGPT")
 llm = ChatOpenAI(
     temperature=0.1,
     model="gpt-3.5-turbo-1106",
+    # Model의 실시간 답변을 보면서, 퀴즈를 어떻게 생성하는지 볼 수 있음
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
 )
 
 # File을 토큰 사이즈에 따라 분류
@@ -58,6 +63,10 @@ with st.sidebar:
             with st.status("Seaching Wikipedia..."):
                 docs = retriever.get_relevant_documents(topic)
 
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
 if not docs:
     st.markdown(
         """
@@ -69,4 +78,45 @@ if not docs:
     """
     )
 else:
-    st.write(docs)
+    prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    """
+        You are a helpful assistant that is role playing as a teacher.
+            
+        Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+        
+        Each question should have 4 answers, three of them must be incorrect and one should be correct.
+            
+        Use (o) to signal the correct answer.
+            
+        Question examples:
+            
+        Question: What is the color of the ocean?
+        Answers: Red|Yellow|Green|Blue(o)
+            
+        Question: What is the capital or Georgia?
+        Answers: Baku|Tbilisi(o)|Manila|Beirut
+            
+        Question: When was Avatar released?
+        Answers: 2007|2001|2009(o)|1998
+            
+        Question: Who was Julius Caesar?
+        Answers: A Roman Emperor(o)|Painter|Actor|Model
+            
+        Your turn!
+            
+        Context: {context}
+    """,
+                )
+            ]
+        )
+
+    chain = {"context": format_docs} | prompt | llm
+
+    # 버튼 생성   
+    start = st.button("Generate Quiz")
+
+    if start:
+        chain.invoke(docs)
