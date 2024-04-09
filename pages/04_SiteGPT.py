f8fb98c996253c7c03a9ea78a131274b7fac05c0
paqj <changraven@gmail.com>
2024-04-09 10:12:56 +0900

    # 10.1 AsyncChromiumLoader - 동적Site crawling

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,3 +1,37 @@
 import streamlit as st
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import Html2TextTransformer
 
-st.title("SiteGPT")
+# GPT가 특정 Site를 크롤링하고 그 정보로 알려줌.
+# 1. playwright, chromimum
+# 2. site loader
+
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
+# HTML을 Text로 변경
+html2text_transformer = Html2TextTransformer()
+
+st.markdown(
+    """
+    # SiteGPT
+            
+    Ask questions about the content of a website.
+            
+    Start by writing the URL of the website on the sidebar.
+"""
+)
+
+with st.sidebar:
+    url = st.text_input("Write down a URL", placeholder="https://example.com")
+
+
+if url:
+    # Crawling
+    loader = AsyncChromiumLoader([url])
+    docs = loader.load()
+    # HTML 태그 없이 content만
+    transformed = html2text_transformer.transform_documents(docs)
+    st.write(docs)
